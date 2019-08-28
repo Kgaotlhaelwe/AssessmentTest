bb5c37fbe2b6f89f13307f3ae227718c2a8b4dab
Kgaotlhaelwe <Kgaotlhaelwe@gmail.com>
2019-08-28 07:48:14 +0200

    fixing

--- client/src/App.js
+++ client/src/App.js
@@ -26,10 +26,10 @@ class App extends Component {
 
     <Provider store={store}>
       <Router> 
-    <div className="App">
+    <div >
 
       <Switch>
-      <Route exact path='/' component={Form} />
+      <Route exact path='/login' component={Form} />
       <Route exact path='/home' component={Home} />
       </Switch>
     
